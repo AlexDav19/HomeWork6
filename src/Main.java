811aebb39d4e2fecb24fe390ba9c624c79302e38
aleks <aleksei-davletov@mail.ru>
2023-06-12 15:04:43 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,54 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();
+        task2();
+        task3();
+        task4();
+        task5();
+        task6();
+        task7();
+        task8();
+    }
+
+    public static void task1() {
+        System.out.println("Задание 1");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+
+    public static void task2() {
+        System.out.println("Задание 2");
+
+    }
+
+    public static void task3() {
+        System.out.println("Задание 3");
+
+    }
+
+    public static void task4() {
+        System.out.println("Задание 4");
+
+    }
+
+    public static void task5() {
+        System.out.println("Задание 5");
+
+    }
+
+    public static void task6() {
+        System.out.println("Задание 5");
+
+    }
+
+    public static void task7() {
+        System.out.println("Задание 5");
+
+    }
+
+    public static void task8() {
+        System.out.println("Задание 5");
+
+    }
+}
